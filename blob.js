dfb3023dc9d9670e1dc724c0a4a170cca1f0f40c
Godeta <Godeta61@gmail.com>
2020-09-24 22:17:16 +0200

    première version fonctionelle, gros blob mange petits

--- blob.js
+++ blob.js
@@ -2,8 +2,31 @@ function Blob(x, y, r) {
   this.pos = createVector(x, y);
   this.r = r;
 
-  this.show = function() {
+  this.show = function () {
     fill(255);
     ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+  }
+  this.update = function () {
+    var mouse = createVector(mouseX, mouseY);
+    //on va en direction d'un vecteur qui lie la position de la souris par rapport à la position de notre boule
+    //mouse.sub(this.pos);
+    //comme on a fait un translate pour garder notre blob au milieu 
+    mouse.sub(width / 2, height / 2);
+    //le vecteur aura constamment la vitesse de 3
+    mouse.setMag(4);
+    this.pos.add(mouse);
+  }
+  this.eat = function (other) {
+    var d = p5.Vector.dist(this.pos, other.pos);
+    //si la distance entre les deux blobs est inférieur au rayo nde l'un + le rayon de l'autre
+    if (d < this.r + other.r) {
+      //this.r += other.r/5; fonctionne mais y'a mieux
+      //on ajoute l'aire
+      var sum = PI * this.r * this.r + PI * other.r * other.r;
+      this.r = sqrt(sum / PI);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//19min 39, clean l'effet de dezoom !!!
 var blob;
 
 var blobs = [];
@@ -6,8 +6,9 @@ var zoom = 1;
 
 function setup() {
   createCanvas(600, 600);
-  blob = new Blob(width/2, height/2, 64);
+  blob = new Blob(0, 0, 64);
   for (var i = 0; i < 50; i++) {
+    //on créer aussi des particules en dehors de la fenêtre vu qu'elle se déplace en fonctione de notre position avec translate
     var x = random(-width, width);
     var y = random(-height, height);
     blobs[i] = new Blob(x, y, 16);
@@ -16,12 +17,21 @@ function setup() {
 
 function draw() {
   background(0);
-
+  //on déplace l'origine de notre canvas pour toujours voir notre blob au centre et donner un effet de mouvement cool
+  translate(width / 2, height / 2); //au milieu
+  //scale pour dezoomer à mesure que notre blob grossi
+  scale(64 / blob.r);
+  translate(-blob.pos.x, -blob.pos.y);
+  //for qui part de la fin pour ne pas avoir de problème avec le split
   for (var i = blobs.length - 1; i >= 0; i--) {
     blobs[i].show();
+    if (blob.eat(blobs[i])) {
+      blobs.splice(i, 1);
+    }
   }
 
 
   blob.show();
+  blob.update();
 
 }
